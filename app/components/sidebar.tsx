8aa6940fe129d6de1398bc1918f07a6d2fe05809
chacklong <3066199080@qq.com>
2024-03-25 13:58:09 +0800

    modify title and topic

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          Long Ai
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          An AI Assistant Built Based on NextGPT.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
